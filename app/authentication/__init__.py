aa17442f8c1c610edcf17da3da9325ef189e1a45
Abou Bakr Frag Mohamed Bahi Eldeen <bakr.frag@gmail.com>
2023-12-12 12:29:24 +0200

    add http authorization using JWT Bearer tokens

---
+++ app/authentication/__init__.py
@@ -0,0 +1,3 @@
+SECRET_KEY = "!sd+(p=gwjrjsbe-(a#nl!gox0t#fq94@f6u#no+81$3qmx&gm"
+SCHEMA = "Bearer"
+ALGORITHM = "HS256"
--- app/authentication/jwt_token.py
+++ app/authentication/jwt_token.py
@@ -1,15 +1,11 @@
 from typing import Dict 
 import jwt 
 import time 
-
-SECRET_KEY = "!sd+(p=gwjrjsbe-(a#nl!gox0t#fq94@f6u#no+81$3qmx&gm"
-SCHEMA = "Bearer"
-
+from . import SECRET_KEY , ALGORITHM
 
  
 class JWTToken(object):
-    secret_key = SECRET_KEY
-    algorithm = "HS256"
+
     def __init__(self):
         self.payload = {
             "user_id":None , 
@@ -30,7 +26,7 @@ class JWTToken(object):
         get jwt token 
         """
         self.payload["user_id"]=user_id
-        token = jwt.encode(self.payload, self.secret_key, algorithm=self.algorithm)
+        token = jwt.encode(self.payload, self.secret_key, algorithm=ALGORITHM)
         return token
 
 
@@ -42,8 +38,8 @@ class JWTToken(object):
             
             decoded_token = jwt.decode(
                 token, 
-                self.secret_key, 
-                algorithms=[self.algorithm])
+                SECRET_KEY, 
+                algorithms=[ALGORITHM])
             return decoded_token if decoded_token.get("expires") > time.time() else None
 
         except Exception as E:
